59d1a3e97dc72a36f01e3a17133f86a9616474b5
Kushagra S <kali@kali.teamkushagra.tech>
2023-09-27 14:20:36 +0530

    Planned commit at Wed Sep 27 14:20:36 IST 2023

--- backup_my_code.sh
+++ backup_my_code.sh
@@ -7,8 +7,8 @@ git commit -m "Planned commit at $time_of_commit"
 git push origin master
 if [ "$?" == 0  ]
 then
-	sed -i "$aSuccessful commit to git branch at $time_of_commit" commit_logs
+	echo "Successful commit to git branch at $time_of_commit" >> commit_logs
 else
-	sed -i "$aUnsuccessfull commit to git branch at $time_of_commit" commit_logs
+	echo "Unsuccessfull commit to git branch at $time_of_commit" >>  commit_logs
 fi
 
