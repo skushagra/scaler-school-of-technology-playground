5340e780f60a590d3147b9e8a9a1870ee229277c
Kushagra S <kali@kali.teamkushagra.tech>
2023-09-27 14:08:43 +0530

    Planned commit at Wed Sep 27 14:08:43 IST 2023

--- backup_my_code.sh
+++ backup_my_code.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 cd "/home/kali/System/Scaler School of Technology"
-echo "Hello"
+git add -A
+time_of_commit=$(date)
+git commit -m "Planned commit at $time_of_commit"
+git push origin master
+if [ "$?" == 0  ]
+then
+	sed -i "$aSuccessful commit to git branch at $time_of_commit" commit_logs
+else
+	sed -i "$aUnsuccessfull commit to git branch at $time_of_commit" commit_logs
+fi
+
