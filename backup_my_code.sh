cbd35ba9bc21dd5705e2f496986f98618cc83cd4
skushagra <kushagra.rigel@gmail.com>
2023-10-04 09:16:10 +0530

    Planned commit at Wed Oct  4 09:16:10 IST 2023

--- backup_my_code.sh
+++ backup_my_code.sh
@@ -7,6 +7,7 @@ git commit -m "Planned commit at $time_of_commit"
 git push origin master
 if [ "$?" == 0  ]
 then
+	echo $?
 	echo "Successful commit to git branch at $time_of_commit" >> commit_logs
 else
 	echo "Unsuccessfull commit to git branch at $time_of_commit" >>  commit_logs
